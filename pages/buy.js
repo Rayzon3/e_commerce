4c031ae9878fbd5771e1783de7b17b44e72ba794
Rayzon3 <rahul.03111999@gmail.com>
2021-11-02 08:15:26 +0530

    dialog color needs to be fixed

--- pages/buy.js
+++ pages/buy.js
@@ -147,17 +147,17 @@ export default function Home() {
           className="fixed inset-0 z-10 overflow-y-auto bg-white"
           onClose={closeModal}
         >
-          <div className="min-h-screen px-4 text-center bg-white">
+          <div className="min-h-screen px-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-100"
+              enterFrom="opacity-0"
               enterTo="opacity-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100"
-              leaveTo="opacity-100"
+              leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-100" />
+              <Dialog.Overlay className="fixed inset-0 hidden w-full h-full overflow-y-auto bg-gray-600 bg-opacity-50" />
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
@@ -170,13 +170,13 @@ export default function Home() {
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-100 scale-95"
-              enterTo="opacity-100 scale-100"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-10 scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-100 scale-95"
+              leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform shadow-xl rounded-2xl">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,24 @@ module.exports = {
   theme: {
     backgroundColor: theme => ({
       'midNight': '#243443',
-      'green': '#2DF6AE'
+      'green': '#2DF6AE',
+      'white':'#FFFFFF',
+      'gray': '#808080'
     }),
     textColor: theme => ({
       'midNight': '#243443',
       'green': '#2DF6AE',
       'white':'#FFFFFF'
     }),
-    extend: {},
+    extend: {
+      backgroundOpacity: {
+        '0': '0',
+        '10': '0.1',
+        '20': '0.2',
+        '95': '0.95',
+        '100': '1',
+      }
+    },
   },
   variants: {
     extend: {},
