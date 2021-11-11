f9bed6159722cc49ce2d60c8fdfb268493bd42fb
SaaranshShandilya <saaranshshandilya@gmail.com>
2021-11-12 00:22:04 +0530

    Image get added

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,30 +1,21 @@
 import uuid
 from django.db import models
 from django.db.models.fields.files import ImageField
-import base64
-import requests
-import json 
+
+def upload_to(instance, filename):
+    return 'post/{filename}'.format(filename=filename)
+
 
 class Product (models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     desc = models.TextField()
     price  =  models.IntegerField()
-    image  = models.ImageField(upload_to='./photos', default = '', blank=True)
+    image  = models.ImageField(upload_to=upload_to, default = '', blank=True, null=True)
     color = models.CharField(max_length=20, blank=True)
-    # image_url = models.TextField(default='',blank=True)
-
-    # def save(self):
-    #     encodedString = base64.b64encode(self.item_image.file.read())
-    #     data = {"key": "2541c62d9518472b87496c3fa37760fc" , "image": encodedString.decode("utf-8")}
-    #     uploadedImageInfo = requests.post("https://api.imgbb.com/1/upload", data=data)
-    #     jsonResponse = json.loads(uploadedImageInfo.text)
-    #     self.item_image_url = jsonResponse["data"]["display_url"]
-    #     super().save()
 
     def __str__(self):
         return self.title
 
 # Create your models here.
 
-#images to be added 
