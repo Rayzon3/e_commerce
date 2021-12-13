2571152e7c45484fd05d9e54c26203f90e3d5cd1
SaaranshShandilya <saaranshshandilya@gmail.com>
2021-12-13 14:03:27 +0530

    Changed pages

--- pages/sell.js
+++ pages/sell.js
@@ -22,7 +22,7 @@ const sell = () => {
           name: title,
           desc : desc,
           price : price,
-          //image : image,
+          image : image,
           headers : headers,
           
       }).then(res =>(console.log(res)))
@@ -47,30 +47,30 @@ const sell = () => {
         <link rel="icon" href="/tree.ico" />
       </Head>
       <NavBar/>
-      <div className="w-1/2 mt-20 mb-20 mx-auto border p-6 rounded-xl   ">
+      <div className="w-1/2 mt-20 mb-20 mx-auto border p-6 rounded-xl border-green bg-white   ">
                 <form onSubmit = {(e)=>handleSubmit(e)}>
-                    <h1 className="text-green text-center text-4xl font-bold">Enlist your product!</h1>
+                    <h1 className="text-midNight text-center text-4xl font-bold">Enlist your product!</h1>
                     <div className="flex flex-col p-3 mt-3">
                     <div className="mb-1">
-                    <label for="title" className="text-white text-2xl" >Name:</label>
+                    <label for="title" className="text-midNight text-2xl" >Name:</label>
                     </div>
-                    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className=" w-full py-1 px-3 border focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent rounded" placeholder="Title of your product" id="title" name="title"></input>
+                    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className=" w-full py-1  px-3 text-midNight border focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent rounded" placeholder="Title of your product" id="title" name="title"></input>
                     </div>
                     <div className="p-3">
                     <div className="mb-1">
-                    <label for="desc" className="text-white text-2xl" >Description:</label>
+                    <label for="desc" className="text-midNight text-2xl" >Description:</label>
                     </div>
                     <textarea type="text" value={desc} id="description" onChange={(e)=>setDesc(e.target.value)} rows="4" className="w-full py-1 px-3 border focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent rounded" id="desc" placeholder="Describe your product..." name="desc"></textarea>
                     </div>
                     <div className="p-3">
                     <div className="mb-1">
-                    <label for="price" className="text-white text-2xl" >Price:</label>
+                    <label for="price" className="text-midNight text-2xl" >Price:</label>
                     </div>
                     <input type="number" value={price} onChange={(e)=>setPrice(e.target.value)} className="w-full py-1 px-3 border focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent rounded" id="price" placeholder="Enter the price" name="price"></input>
                     </div>
                     <div className="p-3">
                     <div className="mb-1">
-                    <label for="img" className="text-white text-2xl" >Upload image of the product:</label>
+                    <label for="img" className="text-midNight text-2xl" >Upload image of the product:</label>
                     </div>
                     <input type="file" value={image} onChange={(e)=>setImage(e.target.value)} accept="image/*" className="w-full py-1 px-3 mt-1 text-white" id="img" placeholder="Enter the price" name="img"></input>
                     </div>
