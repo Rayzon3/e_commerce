888244cf78d5cdc21b995bbae1b50169c21decca
Rahul Bhardwaj <rahul.03111999@gmail.com>
2021-12-18 12:13:53 +0530

    updated

--- pages/buy.js
+++ pages/buy.js
@@ -8,6 +8,89 @@ import axios from "axios";
 import next from "next";
 
 
+// const products = [
+//   {
+//     id: 1,
+//     name: "Basic Tee",
+//     href: "#",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+//     imageAlt: "Front of men's Basic Tee in black.",
+//     price: "₹3500",
+//     color: "Black",
+//   },
+//   {
+//     id: 2,
+//     name: "Nomad Tumbler",
+//     href: "#",
+//     price: "$35",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+//     imageAlt:
+//       "Olive drab green insulated bottle with flared screw lid and flat top.",
+//   },
+//   {
+//     id: 3,
+//     name: "Focus Paper Refill",
+//     href: "#",
+//     price: "$89",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+//     imageAlt:
+//       "Person using a pen to cross a task off a productivity paper card.",
+//   },
+//   {
+//     id: 4,
+//     name: "Machined Mechanical Pencil",
+//     href: "#",
+//     price: "$35",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+//     imageAlt:
+//       "Hand holding black machined steel mechanical pencil with brass tip and top.",
+//   },
+//   {
+//     id: 5,
+//     name: "Basic Tee",
+//     href: "#",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+//     imageAlt: "Front of men's Basic Tee in black.",
+//     price: "₹3500",
+//     color: "Black",
+//   },
+//   {
+//     id: 6,
+//     name: "Nomad Tumbler",
+//     href: "#",
+//     price: "$35",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+//     imageAlt:
+//       "Olive drab green insulated bottle with flared screw lid and flat top.",
+//   },
+//   {
+//     id: 7,
+//     name: "Focus Paper Refill",
+//     href: "#",
+//     price: "$89",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+//     imageAlt:
+//       "Person using a pen to cross a task off a productivity paper card.",
+//   },
+//   {
+//     id: 8,
+//     name: "Machined Mechanical Pencil",
+//     href: "#",
+//     price: "$35",
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+//     imageAlt:
+//       "Hand holding black machined steel mechanical pencil with brass tip and top.",
+//   },
+// ];
+
 export default function Home() {
   let [isOpen, setIsOpen] = useState(false);
   const [product, setProduct] = useState([]);
